4746aeb4b7980f96500505de3fe90fad90cf0ddf
Treehouseman <treehouseman@thegeekgroup.org>
2016-12-29 10:19:12 -0500

    Update tree.cc

--- trunk-recorder/ncurses/tree.cc
+++ trunk-recorder/ncurses/tree.cc
@@ -1220,6 +1220,8 @@ std::string Tree::TTimeParse(int stime){
 	}
 	std::stringstream ttp;
 	if(stime >= 604800){
+		if(stime/60/60/24/7 < 10)
+			tpp << " ";
 		ttp << stime/60/60/24/7 << ":";
 		if(stime/60/60/24<10){
 			ttp << "0" << stime/60/60/24%7 << ":";
