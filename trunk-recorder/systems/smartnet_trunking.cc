5bf91099d842af81b77ed4b121003caf08b10af8
tripp <treehouseman@thegeekgroup.org>
2017-01-28 22:57:09 -0500

    No more boost for smartnet_trunking.cc

--- trunk-recorder/systems/smartnet_trunking.cc
+++ trunk-recorder/systems/smartnet_trunking.cc
@@ -103,5 +103,5 @@ void smartnet_trunking::tune_offset(double f) {
   chan_freq = f;
   int offset_amount = (f - center_freq);
   prefilter->set_center_freq(offset_amount); // have to flip this for 3.7
-  BOOST_LOG_TRIVIAL(info) << "Offset set to: " << offset_amount << " Freq: " << chan_freq;
+  //BOOST_LOG_TRIVIAL(info) << "Offset set to: " << offset_amount << " Freq: " << chan_freq;
 }
